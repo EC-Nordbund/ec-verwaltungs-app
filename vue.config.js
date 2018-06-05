175b8f6c3ea72bb127d198bd79df9a874ec52033
Sebastian <2pi_r2@gmx.de>
2018-06-05 02:55:36 +0200

    testing

--- vue.config.js
+++ vue.config.js
@@ -1,19 +1,9 @@
-console.log(process.env.NODE_ENV)
-
 module.exports = {
-  baseUrl: process.env.NODE_ENV === 'development'?undefined:'./',
+  baseUrl: process.env.NODE_ENV === 'development' ? undefined : './',
   outputDir: 'electron/src/renderer',
   runtimeCompiler: false,
   productionSourceMap: false,
-  // devServer: {
-  //   open: process.platform === 'darwin',
-  //   disableHostCheck: false,
-  //   host: '0.0.0.0',
-  //   port: 8080,
-  //   https: false,
-  //   hotOnly: false,
-  //   // See https://github.com/vuejs/vue-cli/blob/dev/docs/cli-service.md#configuring-proxy
-  //   proxy: null, // string | Object
-  //   before: app => { }
-  // },
+  devServer: {
+    port: 8080
+  }
 }
