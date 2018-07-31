c616a457862cb4b29bd65d743f33c50223c628fc
Sebastian Krüger <2pi_r2@gmx.de>
2018-07-31 23:07:04 +0200

    Update template-details.vue

--- template-details.vue
+++ template-details.vue
@@ -82,7 +82,8 @@ import event from '@/plugins/eventbus'
         }
       })
       .then((v: any) => {
-        ;(<any>this).data = v.data(<any>this).variabels = {
+        ;(<any>this).data = v.data;
+        (<any>this).variabels = {
           authToken: auth.authToken,
           personID: to.params.id
         }
