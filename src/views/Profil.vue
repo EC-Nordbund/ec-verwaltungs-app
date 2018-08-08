5679e025076ff426e2c3db770857dfaa64e2b1c9
Sebastian Krüger <2pi_r2@gmx.de>
2018-08-08 21:24:08 +0200

    grobes Layout

--- src/views/Profil.vue
+++ src/views/Profil.vue
@@ -2,7 +2,13 @@
   <ec-wrapper title="Profil" mini @share="share">
     <template>
       <v-card-text>
-          
+        <v-switch label="Dark-Theme"/>          
+        <v-text-field label="E-Mail"/>
+        <v-text-field label="SMTP-Server"/>
+        <v-text-field label="SMTP-Port"/>
+        <v-text-field label="IMAP-Server"/>
+        <v-text-field label="IMAP-Port"/>
+        <v-text-field label="E-Mail Passwort"/>
       </v-card-text>
     </template>
 
@@ -19,10 +25,8 @@
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator'
 
-
 @Component({})
 export default class admin extends Vue {
-
   share(share: (url: string) => void) {
     share(this.$route.fullPath)
   }
