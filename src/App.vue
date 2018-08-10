73ca7f6cd83f651b30014edaf864aee8b478b56e
Sebastian Krüger <2pi_r2@gmx.de>
2018-08-10 15:12:54 +0200

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -120,7 +120,7 @@ export default class App extends Vue {
   version: string = version
   dark: boolean = false
   soonLogOut: boolean = false
-  countdown: null|Timer = null
+  countdown: null|NodeJs.Timer = null
   nav = nav
   auth = auth
   event = event
