f158a5286275bcf7009378a3c4f7a2a11ac64388
Sebastian Krüger <2pi_r2@gmx.de>
2018-08-16 20:16:17 +0200

    error handler test

--- src/plugins/apollo.ts
+++ src/plugins/apollo.ts
@@ -3,6 +3,9 @@ import VueApollo from 'vue-apollo'
 import { ApolloClient } from 'apollo-client'
 import { HttpLink } from 'apollo-link-http'
 import { InMemoryCache } from 'apollo-cache-inmemory'
+import { onError } from "apollo-link-error";
+import { ApolloLink } from 'apollo-link'
+
 
 // Install Plugin
 Vue.use(VueApollo)
@@ -13,14 +16,17 @@ let client: null | ApolloClient<any> = null
 export function getClient() {
   if (client === null) {
     client = new ApolloClient({
-      link: new HttpLink({
-        uri:
-          'https://h2778646.stratoserver.net:4000/graphql'
-        // uri: 'https://localhost:4000/graphql'
-      }),
+      link: ApolloLink.from([
+        new HttpLink({
+          uri:
+            'https://h2778646.stratoserver.net:4000/graphql'
+          // uri: 'https://localhost:4000/graphql'
+        })
+      ]),
       cache: new InMemoryCache(),
       connectToDevTools: true
     })
+  
   }  
   return client
 }
