dc0107380667077b4ec4c113fe0fc84cf64865f9
Sebastian Krüger <2pi_r2@gmx.de>
2018-12-23 00:39:23 +0100

    add some gql components

---
+++ src/gql/ecGQLquery.vue
@@ -0,0 +1,73 @@
+<template>
+  <div>
+    <slot :data="data" :loading="loading" :reloading="reloading" :errored="errored"/>
+  </div>
+</template>
+<script lang="ts">
+import {
+  Component,
+  Vue,
+  Prop,
+  Watch
+} from 'vue-property-decorator'
+
+import auth from '@/plugins/auth'
+import gql from 'graphql-tag'
+import { getClient } from '@/plugins/apollo'
+
+import electron from '@/plugins/electron'
+
+@Component({})
+export default class ecGQLquery extends Vue {
+  auth = auth
+
+  @Prop({ type: Object, default: () => ({}) })
+  variables!: any
+
+  query: any
+
+  data: any = {}
+  errored = false
+  reloading = false
+  loading = true
+
+  @Watch('variables', { immediate: true })
+  async doQuery() {
+    if (!this.query) {
+      this.query = gql`
+        ${this.$slots.query[0].text}
+      `
+    }
+
+    if (!this.loading) {
+      this.reloading = true
+    }
+
+    const data = await getClient()
+      .query({
+        query: this.query,
+        variables: {
+          ...this.variables,
+          authToken: auth.authToken
+        }
+      })
+      .catch(err => {
+        electron.remote.dialog.showErrorBox(
+          'Bei der Abfrage von Daten ist ein Fehler aufgetreten!',
+          err.toString()
+        )
+        this.errored = true
+        return null
+      })
+    if (data) {
+      this.loading = false
+      this.reloading = false
+      this.data = data.data
+    }
+  }
+
+  public refetch(){
+   this.doQuery()
+  }
+}
+</script>
--- src/main.ts
+++ src/main.ts
@@ -1,26 +1,28 @@
-import apolloProvider from '@/plugins/apollo'
-import auth from '@/plugins/auth'
-import DSGVO from './views/DSGVO.vue'
-import electron, {
-  isElectron,
-  isProduction
-} from '@/plugins/electron'
-import lesezeichenShow from '@/plugins/lesezeichen/showLesezeichen.vue'
-import lesezeichenToggele from '@/plugins/lesezeichen/addLesezeichen.vue'
-import router from '@/plugins/router/router'
-import Vue from 'vue'
-import wrapper from '@/plugins/wrapper.vue'
-import xButton from '@/plugins/xButton/btn.vue'
-import '@/plugins/updateChecker'
-import '@/plugins/design/vuetify'
-import '@/plugins/lib_extension/componentLib_extension'
-import '@/plugins/lib/componentLib'
-import '@/plugins/qrCode'
-import '@/plugins/router/routeHandler'
-import '@/plugins/design/theme-directives'
-import '@/plugins/auth'
-import '@/plugins/widgets'
-
+import DSGVO from './views/DSGVO.vue';
+import ecGQLmutate from '@/gql/ecGQLmutate.vue';
+import ecGQLquery from '@/gql/ecGQLquery.vue';
+import ecGQLwatch from '@/gql/ecGQLwatch.vue';
+import apolloProvider from '@/plugins/apollo';
+import '@/plugins/auth';
+import auth from '@/plugins/auth';
+import '@/plugins/design/theme-directives';
+import '@/plugins/design/vuetify';
+import electron, { isElectron, isProduction } from '@/plugins/electron';
+import lesezeichenToggele from '@/plugins/lesezeichen/addLesezeichen.vue';
+import lesezeichenShow from '@/plugins/lesezeichen/showLesezeichen.vue';
+import '@/plugins/lib_extension/componentLib_extension';
+import '@/plugins/lib/componentLib';
+import '@/plugins/qrCode';
+import '@/plugins/router/routeHandler';
+import router from '@/plugins/router/router';
+import '@/plugins/updateChecker';
+import '@/plugins/widgets';
+import wrapper from '@/plugins/wrapper.vue';
+import xButton from '@/plugins/xButton/btn.vue';
+import Vue from 'vue';
+Vue.component('gql-query', ecGQLquery)
+Vue.component('gql-watch', ecGQLwatch)
+Vue.component('gql-mutate', ecGQLmutate)
 Vue.component('ec-lesezeichen-add', lesezeichenToggele)
 Vue.component('ec-lesezeichen-show', lesezeichenShow)
 Vue.component('ec-x-btn', xButton)
