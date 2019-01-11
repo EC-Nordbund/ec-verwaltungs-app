25c177048480c717b51385c1ede5655ae89b0a2f
Sebastian Krüger <2pi_r2@gmx.de>
2019-01-11 19:20:59 +0100

    fix

--- packages/app/tests/unit/example.spec.ts
+++ packages/app/tests/unit/example.spec.ts
@@ -1,6 +1,6 @@
-import { expect } from "chai";
-import { shallowMount } from "@vue/test-utils";
-import addButton from "@/lib/buttons/add.vue";
+import addButton from '@/lib/buttons/add.vue';
+import { shallowMount } from '@vue/test-utils';
+import { expect } from 'chai';
 
 describe("HelloWorld.vue", () => {
   it("renders props.msg when passed", () => {
