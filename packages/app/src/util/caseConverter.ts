07df9b822db5d162ffa8669e0075c739d68665bb
Sebastian Krüger <2pi_r2@gmx.de>
2019-03-04 20:50:32 +0100

    fix some whitespace

--- packages/app/src/util/caseConverter.ts
+++ packages/app/src/util/caseConverter.ts
@@ -1,2 +1,2 @@
-export const pascalToBindestrich = 
-  (a: string) => a.replace(/(?:^|\.?)([A-Z])/g, (x: string,y: string) => '-' + y.toLowerCase()).replace(/^-/, '')
+export const pascalToBindestrich =
+  (a: string) => a.replace(/(?:^|\.?)([A-Z])/g, (x: string, y: string) => '-' + y.toLowerCase()).replace(/^-/, '');
