7113ac60f0feaf0c0bc86a5c71a7703c6910ff00
Sebastian Krüger <2pi_r2@gmx.de>
2019-03-06 13:16:56 +0100

    fix

--- packages/app/src/pages/_.route.vue
+++ packages/app/src/pages/_.route.vue
@@ -148,7 +148,7 @@ export default class EcRootIndex extends Vue {
   }
 
   private created() {
-    if (this.$authToken) {
+    if (!this.$authToken) {
       this.$router.push({path: '/login', query: {next: this.$route.fullPath}});
     }
   }
