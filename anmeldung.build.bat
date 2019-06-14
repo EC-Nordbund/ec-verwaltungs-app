6e7ea6f2ae0903962a3c6c944fa3738d20173f3b
Sebastian Krüger <2pi_r2@gmx.de>
2019-06-14 04:39:39 +0200

    build scripts

--- anmeldung.build.bat
+++
@@ -1,9 +0,0 @@
-cd packages
-cd forms
-yarn build
-cd ..
-type .\forms\dist\ecForm.umd.min.js > .\anmeldung\src\assets\ecForm.umd.min.js
-cd anmeldung
-yarn build
-
-echo Du musst die JS Files aus der ./dist/index.html in die ./dist/index.php einfügen!
