6ee8adb5b753a27591fa3e78befcd55148f537b5
Sebastian Krüger <2pi_r2@gmx.de>
2019-08-05 21:17:25 +0200

    Update mysql.ts

--- packages/api/src/schema/mysql.ts
+++ packages/api/src/schema/mysql.ts
@@ -1,6 +1,12 @@
 import { createPool } from 'promise-mysql';
 
-const pool = createPool(eval("require('../../config.json')"))
+const pool = createPool({
+  host: process.env.DB_HOST,
+  database: process.env.DB_DB,
+  user: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORT,
+  connectionLimit: 18
+})
 
 export async function query(sql: string, uid: number = -1) {
   console.log(`${uid}: '${sql}'`)
