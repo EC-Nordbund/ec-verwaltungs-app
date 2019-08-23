36def36fd6af8f630bf91945ac881a6e149e5ab1
Sebastian Krüger <2pi_r2@gmx.de>
2019-08-23 14:46:42 +0200

    better build

--- packages/api/api.ts
+++ packages/api/api.ts
@@ -1,5 +1,5 @@
 import { stringValidator, numberValidator, booleanValidator } from "./validatoren";
-import { connectorBase, register, mutation, query, setAPIClass, inform } from "./decorators";
+import { connectorBase, register, mutation, query, inform } from "./decorators";
 import { Socket } from "socket.io";
 
 
@@ -8,7 +8,7 @@ export class connector extends connectorBase{
     super(isClient, socket)
   }
 
-  @register()
+  @register(connector)
   @inform('anmeldung', 0)
   @mutation('addAnmeldungPayment', [
     new stringValidator('Anmelede ID').required().maxLength(15).minLength(10),
@@ -17,21 +17,21 @@ export class connector extends connectorBase{
   ])
   addAnmeldungPayment(anmeldeID: string, betrag: number, eingang: string):Promise<0> {return}
 
-  @register()
+  @register(connector)
   @inform('aks')
   @mutation('addArbeitskreis', [
     new stringValidator('Bezeichnung').required().maxLength(100).minLength(3)
   ])
   addArbeitskreis(bezeichnung:string):Promise<number> {return}
 
-  @register()
+  @register(connector)
   @inform('orgas')
   @mutation('addOrganisation', [
     new stringValidator('Bezeichnung').required().maxLength(100).minLength(3)
   ])
   addOrganisation(bezeichnung:string):Promise<number> {return}
 
-  @register()
+  @register(connector)
   @inform('personen')
   @mutation('addPerson', [
     new stringValidator('Vorname').required().maxLength(50),
@@ -41,7 +41,7 @@ export class connector extends connectorBase{
   ])
   addPerson(vorname: string, nachname: string, gebDat: string, geschlecht: 'm'|'w'):Promise<number> {return}
 
-  @register()
+  @register(connector)
   @inform('person', 0)
   @mutation('addPersonAdresse', [
     new numberValidator('Person ID').required().ganz().min(1),
@@ -51,7 +51,7 @@ export class connector extends connectorBase{
   ])
   addPersonAdresse(personID: number, strasse: string, plz: string, ort: string):Promise<number> {return}
 
-  @register()
+  @register(connector)
   @inform('person', 0)
   @inform('ak', 1)
   @mutation('addPersonArbeitskreisStatus', [
@@ -62,7 +62,7 @@ export class connector extends connectorBase{
   ])
   addPersonArbeitskreisStatus(personID: number, akID: number, date: string|null, status: number):Promise<0> {return}
 
-  @register()
+  @register(connector)
   @inform('person', 0)
   @mutation('addPersonFZ', [
     new numberValidator('Person ID').required().ganz().min(1),
@@ -72,7 +72,7 @@ export class connector extends connectorBase{
   ])
   addPersonFZ(personID: number, gesehenAm: string, fzVom: string, kommentar: string):Promise<0>{return}
 
-  @register()
+  @register(connector)
   @inform('person', 1)
   @mutation('addPersonJuLeiCa', [
     new stringValidator('JuLeiCaNr.').required().numeric().exactLength(11),
@@ -81,7 +81,7 @@ export class connector extends connectorBase{
   ])
   addPersonJuLeiCa(juLeiCaNr: string, personID:number, gueltigBis: string):Promise<0>{return}
 
-  @register()
+  @register(connector)
   @inform('person', 0)
   @mutation('addPersonMail', [
     new numberValidator('Person ID').required().ganz().min(1),
@@ -89,7 +89,7 @@ export class connector extends connectorBase{
   ])
   addPersonMail(personID: number, email: string):Promise<number> {return}
 
-  @register()
+  @register(connector)
   @inform('person', 0)
   @mutation('addPersonTag', [
     new numberValidator('Person ID').required().ganz().min(1),
@@ -98,7 +98,7 @@ export class connector extends connectorBase{
   ])
   addPersonTag(personID: number, tagID: number, notiz: string):Promise<number> {return}
 
-  @register()
+  @register(connector)
   @inform('person', 0)
   @mutation('addPersonTelefon', [
     new numberValidator('Person ID').required().ganz().min(1),
@@ -108,4 +108,4 @@ export class connector extends connectorBase{
 
 }
 
-setAPIClass(connector)
+console.log(connector.methods)
